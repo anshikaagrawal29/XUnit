6534efff2395b44292405938cad61067b4b01ca4
Anshika Agrawal <workwithanshika@gmail.com>
2022-05-27 18:22:14 +0530

    Created New console application and different project for unit testing

---
+++ XUnit/UnitTestProjectXunit1/Payment Calculation/PaymentCalculation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
+namespace PaymentCalculation
+{
+    public class Payments
+    {
+        HashSet<Guid> paymentIds = new HashSet<Guid>();
+
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        public bool CheckEvenOdd(int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        //Create payment Id
+        public Guid createPaymentTransaction()
+        {
+            Guid paymentId = new Guid();
+            paymentIds.Add(paymentId);
+
+            return paymentId;
+            
+        }
+
+        //Get payment Id
+        public Guid GetTransaction(Guid paymentId)
+        {
+            if (paymentId != Guid.Empty && !paymentIds.Contains(paymentId))
+            {
+                throw new Exception("Payment Id not found");
+            }
+            else if (paymentId == Guid.Empty)
+            {
+                throw new Exception("Payment Id empty.");  
+            }
+            else
+                return paymentId;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Payments!!");
+
+            Payments p = new Payments();
+            int result = p.sum(1, 2);
+
+            Console.WriteLine(result);
+
+        }
+    }
+
+
+}
+
+
---
+++ XUnit/UnitTestProjectXunit1/UnitTestProjectXunit1/UnitTestUsingAnotherProject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PaymentCalculation;
+
+
+namespace UnitTestProjectXunit1
+{
+    public class UnitTestUsingAnotherProject
+    {
+        Payments p = new Payments();
+
+        [Fact]
+        public void PassedTestSum()
+        {
+            Assert.Equal(2, p.sum(1, 1));
+        }
+
+        [Fact]
+        public void PassedCheckEvenAndOdd()
+        {
+            Assert.True(p.CheckEvenOdd(2));
+        }
+
+        [Fact]
+        public void FailedCheckEvenAndOdd()
+        {
+            Assert.True(p.CheckEvenOdd(3));
+        }
+
+        [Fact]
+        public void PassedCheckIdExists()
+        {
+            Guid id = p.createPaymentTransaction();
+            Guid retreivedId = p.GetTransaction(id);
+            Assert.Equal(id, retreivedId);
+        }
+
+        // Does not contains check
+        [Fact]
+        public void FailedCheckIdExists()
+        {
+            Guid id = new Guid();
+            try
+            {
+                Guid retreivedId = p.GetTransaction(id);
+                Assert.Equal(id, retreivedId);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            
+        }
+
+        // empty check
+        [Fact]
+        public void EmptyPaymentIdCheck()
+        {
+            Guid id = Guid.Empty;
+            
+            try
+            {
+                Guid retreivedId = p.GetTransaction(id);
+            }
+            catch(Exception e)
+            {
+                if (e.Message == "Payment Id empty.")
+                { 
+                    throw new Exception("Payment Id empty inside catch");
+                }
+            }
+            
+        }
+
+    }
+}
